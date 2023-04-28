8d75e9c89c1055df85b461d783eea33e49878eeb
JokoSableng <linggar.bagas10@gmail.com>
2023-04-28 13:33:32 +0700

    print melbba

--- application/views/admin/Transaksi_PesananLBBA/Transaksi_PesananLBBA.php
+++ application/views/admin/Transaksi_PesananLBBA/Transaksi_PesananLBBA.php
@@ -114,41 +114,34 @@
 
 <!-- Modal -->
 <div class="modal fade" id="melbbaModal" tabindex="-1" role="dialog" aria-labelledby="periodeLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document" style="max-width: 250px">
+    <div class="modal-dialog modal-lg" role="document" style="max-width: 550px">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="periodeLabel"> <i class="fas fa-cogs"></i> Ganti Periode</h5>
+                <h5 class="modal-title" id="periodeLabel"> <i class="fas fa-cogs"></i> </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form method="post" action="<?php echo base_url('admin/dashboard/ganti_periode') ?>" class="user">
-                    <div class="form-group">
-                        <input type="text" class="form-control form-control-user" list="month" id="bulanPeriode" placeholder="Pilih Bulan..." name="bulan">
-                        <datalist id="month">
-                            <option value='01'>01</option>
-                            <option value='02'>02</option>
-                            <option value='03'>03</option>
-                            <option value='04'>04</option>
-                            <option value='05'>05</option>
-                            <option value='06'>06</option>
-                            <option value='07'>07</option>
-                            <option value='08'>08</option>
-                            <option value='09'>09</option>
-                            <option value='10'>10</option>
-                            <option value='11'>11</option>
-                            <option value='12'>12</option>
-                        </datalist>
+                    <div class="form-group" style="text-align:center">
+                        <h3>Apakah anda akan cetak <br/><Br/><span style="font-weight:bold;font-size:18;" id="sp1"></span><br/></h3>
+						<br/>
+						<div style="text-align:center;color:blue;">
+						Disimpan oleh <span id="sp2"></span></div>
+						<br/>
+						<div style="text-align:center;color:red">
+							Yang sudah validasi:<br/>
+							<div id="sp3"></div>
+						</div>
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="tahunPeriode" placeholder="Tahun..." name="tahun">
-                    </div>
+                    
 
-                    <button class="btn btn-primary btn-user btn-block">Ubah Periode</button>
+                   
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+				 <button id="btnPrint" data-dismiss="modal" class="btn btn-primary ">Cetak</button>
             </div>
             </form>
         </div>
@@ -157,6 +150,74 @@
 
 <script type="text/javascript">
     $(document).ready(function() {
+        $('#melbbaModal').on('show.bs.modal', function (event) {
+			var a = event.relatedTarget;
+			var no = $(a).data("no");
+			$("#sp1").text(no);
+			var ttd1 = $(a).data("ttd1");
+			var ttd1d = $(a).data("ttd1d");
+			$("#sp2").text(ttd1 + " pada tanggal " + ttd1d);
+			var lengkap = 1;
+			var hist = "";
+			var ttd2 = $(a).data("ttd2");
+			var ttd2d = $(a).data("ttd2d");
+			if(ttd2 != "" && ttd2 != null )
+				hist += (ttd2 + " pada tanggal " + ttd2d) + "<Br/>";
+			else lengkap = 0;
+			
+			var ttd2 = $(a).data("ttd2");
+			var ttd2d = $(a).data("ttd2d");
+			if(ttd2 != "" && ttd2 != null )
+				hist += (ttd2 + " pada tanggal " + ttd2d) + "<Br/>";
+			else lengkap = 0;
+			
+			var ttd3 = $(a).data("ttd3");
+			var ttd3d = $(a).data("ttd3d");
+			if(ttd3 != "" && ttd3 != null )
+				hist += (ttd3 + " pada tanggal " + ttd3d) + "<Br/>";
+			else lengkap = 0;
+			
+			var ttd4 = $(a).data("ttd4");
+			var ttd4d = $(a).data("ttd4d");
+			if(ttd4 != "" && ttd4 != null )
+				hist += (ttd4 + " pada tanggal " + ttd4d) + "<Br/>";
+			else lengkap = 0;
+			
+			var ttd5 = $(a).data("ttd5");
+			var ttd5d = $(a).data("ttd5d");
+			if(ttd5 != "" && ttd5 != null )
+				hist += (ttd5 + " pada tanggal " + ttd5d) + "<Br/>";
+			else lengkap = 0;
+			
+			// var ttd6 = $(a).data("ttd6");
+			// var ttd6d = $(a).data("ttd6d");
+			// if(ttd6 != "" && ttd6 != null )
+			// 	hist += (ttd6 + " pada tanggal " + ttd6d) + "<Br/>";
+			// else lengkap = 0;
+			
+			// var ttd7 = $(a).data("ttd7");
+			// var ttd7d = $(a).data("ttd7d");
+			// if(ttd7 != "" && ttd7 != null )
+			// 	hist += (ttd7 + " pada tanggal " + ttd7d) + "<Br/>";
+			// else lengkap = 0;
+			
+			if(lengkap == 0)
+			{
+				$("#btnPrint").hide();
+			}
+			else
+			{
+				var id = $(a).data("id");
+				$("#btnPrint").click(function()
+				{
+					window.open("JASPER/" + id);
+				});
+			}
+			
+			$("#sp3").html(hist);
+			console.log(event.relatedTarget);
+		});
+        
         $('#example').DataTable({
             dom: "<'row'<'col-md-6'><'col-md-6'>>" + // 
                 "<'row'<'col-md-2'l><'col-md-6 test_btn'><'col-md-4'f>>" + // peletakan entries, search, dan test_btn
