8b62ed4ea650044ad9a53151f2a2383c8cf34791
nadiahelvira <131247223+nadiahelvira@users.noreply.github.com>
2023-05-17 15:15:55 +0700

    jj

--- application/controllers/admin/Transaksi_Verifikasi_SPIK.php
+++ application/controllers/admin/Transaksi_Verifikasi_SPIK.php
@@ -18,8 +18,8 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
             );
             redirect('admin/auth');
         }
-        if ($this->session->userdata['menu_sparepart'] != 'mr_order_sample') {
-            $this->session->set_userdata('menu_sparepart', 'mr_order_sample');
+        if ($this->session->userdata['menu_sparepart'] != 'ppc_spik') {
+            $this->session->set_userdata('menu_sparepart', 'ppc_spik');
             $this->session->set_userdata('kode_menu', 'T0036');
             $this->session->set_userdata('keyword_pp', '');
             $this->session->set_userdata('order_pp', 'NO_ID');
@@ -36,7 +36,8 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
         $per = $this->session->userdata['periode'];
         $where = array(
             // 'DR' => $dr,
-            'PER' => $per,
+            'TUJUAN' => $dr,
+            // 'PER' => $per,
             // 'SUB' => 'PSL',
             // 'FLAG' => 'PP',
             // 'FLAG2' => 'SP',
@@ -44,7 +45,7 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
             // 'TYP' => 'RND_PISAU',
         );
         $this->db->select('*');
-        $this->db->from('mr_order_sample');
+        $this->db->from('ppc_spik');
         $this->db->where($where);
         $i = 0;
         foreach ($this->column_search as $item) {
@@ -90,14 +91,15 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
         $per = $this->session->userdata['periode'];
         $where = array(
             // 'DR' => $dr,
-            'PER' => $per,
+            'TUJUAN' => $dr,
+            // 'PER' => $per,
             // 'SUB' => 'PSL',
             // 'FLAG' => 'PP',
             // 'FLAG2' => 'SP',
             // 'VAL' => '0',
             // 'TYP' => 'RND_PISAU',
         );
-        $this->db->from('mr_order_sample');
+        $this->db->from('ppc_spik');
         $this->db->where($where);
         return $this->db->count_all_results();
     }
@@ -107,30 +109,28 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
         $list = $this->get_datatables();
         $data = array();
         $no = @$_POST['start'];
-        foreach ($list as $mr_order_sample) {
-            $JASPER = "window.open('JASPER/" . $mr_order_sample->NO_ID . "','', 'width=1000','height=900');";
+        foreach ($list as $ppc_spik) {
+            $JASPER = "window.open('JASPER/" . $ppc_spik->NO_ID . "','', 'width=1000','height=900');";
             $no++;
             $row = array();
-            $row[] = "<input type='checkbox' class='singlechkbox' name='check[]' value='" . $mr_order_sample->NO_ID . "'>";
+            $row[] = "<input type='checkbox' class='singlechkbox' name='check[]' value='" . $ppc_spik->NO_ID . "'>";
             $row[] = '<div class="dropdown">
                         <a style="background-color: #00b386;" class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fa fa-bars icon" style="font-size: 13px;"></i>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <a class="dropdown-item" href="' . site_url('admin/Transaksi_Verifikasi_SPIK/update/' . $mr_order_sample->NO_ID) . '"> <i class="fa fa-edit"></i> Edit</a>
+                            <a class="dropdown-item" href="' . site_url('admin/Transaksi_Verifikasi_SPIK/update/' . $ppc_spik->NO_ID) . '"> <i class="fa fa-edit"></i> Edit</a>
                             <a name="NO_ID" class="dropdown-item" href="#" onclick="' . $JASPER . '");"><i class="fa fa-print"></i> Print</a>
                         </div>
                     </div>';
             $row[] = $no . ".";
-            $row[] = $mr_order_sample->NO_BUKTI;
-            $row[] = date("d-m-Y", strtotime($mr_order_sample->TGL));
-            $row[] = date("d-m-Y", strtotime($mr_order_sample->TGL_DIMINTA));
-            $row[] = $mr_order_sample->KODE_DEVISI;
-            $row[] = $mr_order_sample->KET;
-            $row[] = $mr_order_sample->JENIS_SAMPLE;
-            $row[] = $mr_order_sample->JENIS_ORDER;
-            $row[] = $mr_order_sample->TUJUAN;
-            // $row[] = "<img src='/Dragon_Sparepart_baru/gambar/pesananpisausample/$mr_order_sample->GAMBAR' width='auto' height='120'>";
+            $row[] = $ppc_spik->no_bukti;
+            $row[] = $ppc_spik->no_order;
+            $row[] = date("d-m-Y", strtotime($ppc_spik->tgl));
+            $row[] = $ppc_spik->gambar1;
+            $row[] = $ppc_spik->dr;
+            $row[] = $ppc_spik->tujuan;
+            // $row[] = "<img src='/Dragon_Sparepart_baru/gambar/pesananpisausample/$ppc_spik->GAMBAR' width='auto' height='120'>";
             $data[] = $row;
         }
         $output = array(
@@ -156,7 +156,7 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
             // 'VAL' => '0',
             // 'TYP' => 'RND_PISAU',
         );
-        $data['mr_order_sample'] = $this->transaksi_model->tampil_data($where, 'mr_order_sample', 'NO_ID')->result();
+        $data['ppc_spik'] = $this->transaksi_model->tampil_data($where, 'ppc_spik', 'NO_ID')->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/navbar');
         $this->load->view('admin/Transaksi_Verifikasi_SPIK/Transaksi_Verifikasi_SPIK', $data);
@@ -166,9 +166,9 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
     public function delete($id)
     {
         $where = array('NO_ID' => $id);
-        $this->transaksi_model->hapus_data($where, 'mr_order_sample');
+        $this->transaksi_model->hapus_data($where, 'ppc_spik');
         $where = array('ID' => $id);
-        $this->transaksi_model->hapus_data($where, 'mr_order_sampled');
+        $this->transaksi_model->hapus_data($where, 'ppc_spikd');
         $this->session->set_flashdata(
             'pesan',
             '<div class="alert alert-danger alert-dismissible fade show" role="alert"> 
@@ -183,61 +183,61 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
 
     // function delete_multiple()
     // {
-    //     $this->transaksi_model->remove_checked('mr_order_sample', 'mr_order_sampled');
+    //     $this->transaksi_model->remove_checked('ppc_spik', 'ppc_spikd');
     //     redirect('admin/Transaksi_PesananPisau/index_Transaksi_Verifikasi_SPIK');
     // }
 
     public function update($id)
     {
-        $q1 = "SELECT mr_order_sample.NO_ID as ID,
-                mr_order_sample.NO_BUKTI AS NO_BUKTI,
-                mr_order_sample.TGL AS TGL,
-                mr_order_sample.TGL_DIMINTA AS TGL_DIMINTA,
-                mr_order_sample.KODE_DEVISI AS KODE_DEVISI,
-                mr_order_sample.KET AS KET,
-                mr_order_sample.JENIS_SAMPLE AS JENIS_SAMPLE,
-                mr_order_sample.JENIS_ORDER AS JENIS_ORDER,
-                mr_order_sample.TUJUAN AS TUJUAN,
-                mr_order_sample.TOTAL_QTY AS TOTAL_QTY,
-                mr_order_sample.VAL AS VAL,
+        $q1 = "SELECT ppc_spik.NO_ID as ID,
+                ppc_spik.NO_BUKTI AS NO_BUKTI,
+                ppc_spik.TGL AS TGL,
+                ppc_spik.TGL_DIMINTA AS TGL_DIMINTA,
+                ppc_spik.KODE_DEVISI AS KODE_DEVISI,
+                ppc_spik.KET AS KET,
+                ppc_spik.JENIS_SAMPLE AS JENIS_SAMPLE,
+                ppc_spik.JENIS_ORDER AS JENIS_ORDER,
+                ppc_spik.TUJUAN AS TUJUAN,
+                ppc_spik.TOTAL_QTY AS TOTAL_QTY,
+                ppc_spik.VAL AS VAL,
                 
-                mr_order_sampled.NO_ID AS NO_ID,
-                mr_order_sampled.REC AS REC,
-                mr_order_sampled.ARTICLE AS ARTICLE,
-                mr_order_sampled.WARNA AS WARNA,
-                mr_order_sampled.OUTSOLE AS OUTSOLE,
-                mr_order_sampled.SIZE AS SIZE,
-                mr_order_sampled.QTY AS QTY,
-                mr_order_sampled.SATUAN AS SATUAN,
-                mr_order_sampled.KET AS KET
-        FROM mr_order_sample, mr_order_sampled 
-        WHERE mr_order_sample.NO_ID=$id 
-        AND mr_order_sample.NO_ID=mr_order_sampled.ID 
-        ORDER BY mr_order_sampled.REC";
+                ppc_spikd.NO_ID AS NO_ID,
+                ppc_spikd.REC AS REC,
+                ppc_spikd.ARTICLE AS ARTICLE,
+                ppc_spikd.WARNA AS WARNA,
+                ppc_spikd.OUTSOLE AS OUTSOLE,
+                ppc_spikd.SIZE AS SIZE,
+                ppc_spikd.QTY AS QTY,
+                ppc_spikd.SATUAN AS SATUAN,
+                ppc_spikd.KET AS KET
+        FROM ppc_spik, ppc_spikd 
+        WHERE ppc_spik.NO_ID=$id 
+        AND ppc_spik.NO_ID=ppc_spikd.ID 
+        ORDER BY ppc_spikd.REC";
 
-        // SELECT mr_order_sample.NO_ID as ID,
-        //         mr_order_sample.NO_BUKTI AS NO_BUKTI,
-        //         mr_order_sample.TGL AS TGL,
-        //         mr_order_sample.TGL_DIMINTA AS TGL_DIMINTA_H,
-        //         mr_order_sample.KODE_DEVISI AS KODE_DEVISI,
-        //         mr_order_sample.KET AS KET,
-        //         mr_order_sample.JENIS_SAMPLE AS JENIS_SAMPLE,
-        //         mr_order_sample.JENIS_ORDER AS JO,
-        //         mr_order_sample.TUJUAN AS TS,
-        //         -- mr_order_sample.TOTAL_QTY AS TOTAL_QTY,
-        //         -- mr_order_sample.VAL AS VAL,
+        // SELECT ppc_spik.NO_ID as ID,
+        //         ppc_spik.NO_BUKTI AS NO_BUKTI,
+        //         ppc_spik.TGL AS TGL,
+        //         ppc_spik.TGL_DIMINTA AS TGL_DIMINTA_H,
+        //         ppc_spik.KODE_DEVISI AS KODE_DEVISI,
+        //         ppc_spik.KET AS KET,
+        //         ppc_spik.JENIS_SAMPLE AS JENIS_SAMPLE,
+        //         ppc_spik.JENIS_ORDER AS JO,
+        //         ppc_spik.TUJUAN AS TS,
+        //         -- ppc_spik.TOTAL_QTY AS TOTAL_QTY,
+        //         -- ppc_spik.VAL AS VAL,
                 
-        //         mr_order_sampled.NO_ID AS NO_ID,
-        //         mr_order_sampled.REC AS REC,
-        //         mr_order_sampled.ARTICLE AS NA_BHN,
-        //         mr_order_sampled.SIZE AS SIZE,
-        //         mr_order_sampled.QTY AS QTY,
-        //         mr_order_sampled.SATUAN AS SATUAN,
-        //         mr_order_sampled.KET AS KET1,
-        //     FROM mr_order_sample,mr_order_sampled 
-        //     WHERE mr_order_sample.NO_ID=$id 
-        //     AND mr_order_sample.NO_ID=mr_order_sampled.ID 
-        //     ORDER BY mr_order_sampled.REC";
+        //         ppc_spikd.NO_ID AS NO_ID,
+        //         ppc_spikd.REC AS REC,
+        //         ppc_spikd.ARTICLE AS NA_BHN,
+        //         ppc_spikd.SIZE AS SIZE,
+        //         ppc_spikd.QTY AS QTY,
+        //         ppc_spikd.SATUAN AS SATUAN,
+        //         ppc_spikd.KET AS KET1,
+        //     FROM ppc_spik,ppc_spikd 
+        //     WHERE ppc_spik.NO_ID=$id 
+        //     AND ppc_spik.NO_ID=ppc_spikd.ID 
+        //     ORDER BY ppc_spikd.REC";
         $data['rnd'] = $this->transaksi_model->edit_data($q1)->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/navbar');
@@ -249,7 +249,7 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
     {
         $delete = $this->input->post('check');
         for ($i = 0; $i < count($delete); $i++) {
-            $this->db->query("UPDATE mr_order_sample SET VAL=1 where no_id=$delete[$i]");
+            $this->db->query("UPDATE ppc_spik SET VAL=1 where no_id=$delete[$i]");
         }
         redirect('admin/Transaksi_Verifikasi_SPIK/index_Transaksi_Verifikasi_SPIK');
     }
@@ -352,39 +352,39 @@ class Transaksi_Verifikasi_SPIK extends CI_Controller
         $PHPJasperXML = new \PHPJasperXML();
         $PHPJasperXML->load_xml_file("phpjasperxml/Transaksi_Validasi_OrderSample.jrxml");
         $no_id = $id;
-        $query = "SELECT mr_order_sample.NO_ID as ID,
-                mr_order_sample.NO_BUKTI AS NO_BUKTI,
-                date_format(mr_order_sample.TGL,'%d-%m-%Y') AS TGL,
-                date_format(mr_order_sample.TGL_DIMINTA,'%d-%m-%Y') AS TGL_DIMINTA,
-                date_format(mr_order_sample.TGL_SELESAI,'%d-%m-%Y') AS TGL_SELESAI,
-                mr_order_sample.KODE_DEVISI AS KODE_DEVISI,
-                mr_order_sample.KET AS KET,
-                mr_order_sample.JENIS_SAMPLE AS JENIS_SAMPLE,
-                mr_order_sample.JENIS_ORDER AS JENIS_ORDER,
-                mr_order_sample.TUJUAN AS TUJUAN,
-                mr_order_sample.TOTAL_QTY AS TOTAL_QTY,
-                mr_order_sample.VAL AS VAL,
-                mr_order_sample.NM_TTD1 AS TTD1_USR, 
-                mr_order_sample.NM_TTD2 AS TTD2_USR, 
-                mr_order_sample.NM_TTD3 AS TTD3_USR,
-                mr_order_sample.TG_TTD1 AS TTD1_SMP,
-                mr_order_sample.TG_TTD2 AS TTD2_SMP,
-                mr_order_sample.TG_TTD3 AS TTD3_SMP,
+        $query = "SELECT ppc_spik.NO_ID as ID,
+                ppc_spik.NO_BUKTI AS NO_BUKTI,
+                date_format(ppc_spik.TGL,'%d-%m-%Y') AS TGL,
+                date_format(ppc_spik.TGL_DIMINTA,'%d-%m-%Y') AS TGL_DIMINTA,
+                date_format(ppc_spik.TGL_SELESAI,'%d-%m-%Y') AS TGL_SELESAI,
+                ppc_spik.KODE_DEVISI AS KODE_DEVISI,
+                ppc_spik.KET AS KET,
+                ppc_spik.JENIS_SAMPLE AS JENIS_SAMPLE,
+                ppc_spik.JENIS_ORDER AS JENIS_ORDER,
+                ppc_spik.TUJUAN AS TUJUAN,
+                ppc_spik.TOTAL_QTY AS TOTAL_QTY,
+                ppc_spik.VAL AS VAL,
+                ppc_spik.NM_TTD1 AS TTD1_USR, 
+                ppc_spik.NM_TTD2 AS TTD2_USR, 
+                ppc_spik.NM_TTD3 AS TTD3_USR,
+                ppc_spik.TG_TTD1 AS TTD1_SMP,
+                ppc_spik.TG_TTD2 AS TTD2_SMP,
+                ppc_spik.TG_TTD3 AS TTD3_SMP,
                 
-                mr_order_sampled.NO_ID AS NO_ID,
-                mr_order_sampled.REC AS REC,
-                mr_order_sampled.ARTICLE AS ARTICLE,
-                mr_order_sampled.WARNA AS WARNA,
-                mr_order_sampled.OUTSOLE AS OUTSOLE,
-                mr_order_sampled.SIZE AS SIZE,
-                mr_order_sampled.JENIS AS JENIS,
-                mr_order_sampled.QTY AS QTY,
-                mr_order_sampled.SATUAN AS SATUAN,
-                mr_order_sampled.KET AS KET2
-        FROM mr_order_sample, mr_order_sampled 
-        WHERE mr_order_sample.NO_ID=$id 
-        AND mr_order_sample.NO_ID=mr_order_sampled.ID 
-        ORDER BY mr_order_sampled.REC";
+                ppc_spikd.NO_ID AS NO_ID,
+                ppc_spikd.REC AS REC,
+                ppc_spikd.ARTICLE AS ARTICLE,
+                ppc_spikd.WARNA AS WARNA,
+                ppc_spikd.OUTSOLE AS OUTSOLE,
+                ppc_spikd.SIZE AS SIZE,
+                ppc_spikd.JENIS AS JENIS,
+                ppc_spikd.QTY AS QTY,
+                ppc_spikd.SATUAN AS SATUAN,
+                ppc_spikd.KET AS KET2
+        FROM ppc_spik, ppc_spikd 
+        WHERE ppc_spik.NO_ID=$id 
+        AND ppc_spik.NO_ID=ppc_spikd.ID 
+        ORDER BY ppc_spikd.REC";
         $PHPJasperXML->transferDBtoArray($servername, $username, $password, $database);
         $PHPJasperXML->arraysqltable = array();
         $result1 = mysqli_query($conn, $query);
